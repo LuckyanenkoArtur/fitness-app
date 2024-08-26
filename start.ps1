dfdc76db4a8d9ccce8c541989aa869d9e4a1ec43
Lukyanena Arthur <lukyanena@gmail.com>
2024-08-26 18:11:28 +0300

    Finish project

--- start.ps1
+++ start.ps1
@@ -7,10 +7,22 @@ docker-compose up -d --build
 # Check if docker-compose was successful
 if ($LASTEXITCODE -eq 0) {
     Write-Host "Docker containers built and started successfully."
-    
+
     # Define the paths
     $clientPath = ".\frontend"
     $serverPath = ".\backend"
+
+    # Function to run yarn install
+    function Install-YarnModules($path) {
+        Set-Location $path
+        Write-Host "Installing yarn modules in $path..."
+        yarn install
+        Set-Location ..
+    }
+
+    # Install modules for client and server
+    Install-YarnModules $clientPath
+    Install-YarnModules $serverPath
 
     # Define the npm options
     $run = "yarn dev"
